21a9eb6ff4e4db7a27f2fda60b3ea35635310429
Zachary Patterson <localhost@zspatter.dev>
2019-03-15 11:21:24 -0400

    Adjusts loop condition in printMonthBody
    This ensures the loop behaves as desired regardless of the month. When the Calendar object increments its month, the loop terminates.

--- Chapter_13/Ex13_04/Ex13_04.java
+++ Chapter_13/Ex13_04/Ex13_04.java
@@ -18,8 +18,9 @@ public class Ex13_04
     private static boolean validateArguments(String[] args)
     {
         if (args.length == 2)
+        {
             return validateMonth(args[0]) && validateYear(args[1]);
-        else if (args.length == 0)
+        } else if (args.length == 0)
             return true;
         else
             return args.length == 1 && validateMonth(args[0]);
@@ -28,19 +29,11 @@ public class Ex13_04
     private static boolean validateMonth(String month)
     {
         return month.matches("0*[1-9]") || month.matches("1[012]");
-
-//            System.out.println("The passed argument of " + month +
-//                    " for month is invalid. Valid months range from 1-12");
-//            System.exit(1);
     }
 
     private static boolean validateYear(String year)
     {
         return year.matches("\\d{4}");
-
-//            System.out.println("The passed argument of " + year +
-//                    " for year is invalid. Valid years must be 4 digits.");
-//            System.exit(1);
     }
 
     private static Calendar buildCalendarObject(String[] args)
@@ -79,12 +72,13 @@ public class Ex13_04
         for (int i = 1; i < calendar.get(Calendar.DAY_OF_WEEK); i++)
             System.out.printf("%4s", padding);
 
-        for (int i = calendar.get(Calendar.DATE); i <= calendar.getActualMaximum(Calendar.DATE); i++, calendar.add(Calendar.DATE, 1))
+        // loop terminates when month changes (jan 31 -> feb 1)
+        for (int i = 1; i == calendar.get(Calendar.DATE); i++, calendar.add(Calendar.DATE, 1))
         {
-            if (calendar.get(Calendar.DAY_OF_WEEK) == 7)
-                System.out.printf("%4d\n", i);
+            if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY)
+                System.out.printf("%4d%n", calendar.get(Calendar.DATE));
             else
-                System.out.printf("%4d", i);
+                System.out.printf("%4d", calendar.get(Calendar.DATE));
         }
         System.out.println();
     }
@@ -98,7 +92,7 @@ public class Ex13_04
 
     private static void printFullYear(Calendar calendar)
     {
-        Calendar tempCal = new GregorianCalendar(calendar.get(Calendar.YEAR), 0, 1);
+        Calendar tempCal = new GregorianCalendar(calendar.get(Calendar.YEAR), Calendar.JANUARY, 1);
 
         for (int i = 0; i < 12; i++)
         {
