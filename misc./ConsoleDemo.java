1c73954a4731a1b23d65f6f1031590737b4dcd76
Zachary Patterson <zach.s.patterson@gmail.com>
2019-03-24 20:50:57 -0400

    Adds blinkd effect to ouput and compiled file

--- misc./ConsoleDemo.java
+++ misc./ConsoleDemo.java
@@ -2,7 +2,7 @@ import java.io.Console;
 
 public class ConsoleDemo
 {
-    private static final String ANSI_GREEN = "\033[1;32m";
+    private static final String ANSI_GREEN = "\033[1;5;32m";
     private static final String ANSI_RESET = "\033[0m";
 
     public static void main(String[] args)
@@ -15,9 +15,9 @@ public class ConsoleDemo
         }
 
         String input = console.readLine("Enter your name: ");
-        System.out.printf("Your name is: %s%s%s\n", ANSI_GREEN, input, ANSI_RESET);
+        System.out.printf("Your name is: %s%s%s\n\n", ANSI_GREEN, input, ANSI_RESET);
 
-        char[] password = console.readPassword("\nEnter your password: ");
-        System.out.printf("Your password is: %s%s%s\n", ANSI_GREEN, String.valueOf(password), ANSI_RESET);
+        char[] password = console.readPassword("Enter your password: ");
+        System.out.printf("Your password is: %s%s%s\n\n", ANSI_GREEN, String.valueOf(password), ANSI_RESET);
     }
 }
