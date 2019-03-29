0a63ac0c470e1846f42a4d6eca0a047fbdef3e9a
Zachary Patterson <localhost@zspatter.dev>
2019-03-29 16:09:52 -0400

    Adds color to output with ANSI codes

--- chapter_12/Ex12_03/Ex12_03.java
+++ chapter_12/Ex12_03/Ex12_03.java
@@ -8,6 +8,8 @@ public class Ex12_03
             "May", "June","July", "August", "September", "October",
             "November", "December"};
     private static int[] dom = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static final String ANSI_ORANGE = "\033[38;5;202m";
+	private static final String ANSI_RESET = "\033[0m";
 
     public static void main(String[] args)
     {
@@ -17,8 +19,7 @@ public class Ex12_03
         {
             String month = months[input - 1];
             int days = dom[input - 1];
-            System.out.println("The month corresponding with your input is "
-                    + month + " which has " + days + " days in it.");
+            System.out.printf("The month corresponding with your input is %s%s%s which has %s%d%s days in it.", ANSI_ORANGE, month, ANSI_RESET, ANSI_ORANGE, days, ANSI_RESET);
         } catch (ArrayIndexOutOfBoundsException e)
         {
             System.out.println("Invalid input.");
